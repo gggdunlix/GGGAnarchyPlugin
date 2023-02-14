cc298fa9a8319593829397187fe9e7d1af6eeeb8
GGG Dunlix <33298379+gggdunlix@users.noreply.github.com>
2023-02-14 09:16:16 -0500

    Update SaveNQuit.java

--- src/main/java/ml/gggrealms/gggmcanarchy/SaveNQuit.java
+++ src/main/java/ml/gggrealms/gggmcanarchy/SaveNQuit.java
@@ -11,9 +11,33 @@ public class SaveNQuit implements CommandExecutor {
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        Boolean playerWantedStatus = false;
+        //Need to get player wanted status from the mongodb
+        Boolean canPlayerLeave = true;
         Player player = (Player) sender;
         Location playerLoc = player.getLocation();
-        sender.sendMessage("Stand Still for 20 seconds to disconnect!");
+        //determine using playerWantedStatus whether or not they are allowed to leave
+        if (playerWantedStatus == false) {
+            canPlayerLeave = true;
+        } else {
+            canPlayerLeave = false;
+        }
+        if (canPlayerLeave = false && playerWantedStatus = true) {
+            sender.sendMessage("You can't save and quit while wanted. Either lose the wanted level, or quit and lose your inventory.");
+        } else if (canPlayerLeave = false && playerWantedStatus = false) {
+            sender.sendMessage("You can't leave the server right now.");
+        } else if (canPlayerLeave = true) {
+            sender.sendMessage("Stand Still for 20 seconds to disconnect!")
+            Thread.sleep(20);
+            if (player.getLocation() == playerLoc) {
+                //disconnect the player!
+            } else {
+                sender.sendMessage("You moved! Try again!");
+            }
+        }
+        
+        
 
 
         return false;
