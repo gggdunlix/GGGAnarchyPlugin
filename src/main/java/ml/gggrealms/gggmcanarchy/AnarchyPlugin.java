6fe0063eb508844195e8ab008c4dd926e4a874b9
GGG Dunlix <33298379+gggdunlix@users.noreply.github.com>
2023-02-16 08:12:07 -0500

    Update AnarchyPlugin.java

--- src/main/java/ml/gggrealms/gggmcanarchy/AnarchyPlugin.java
+++ src/main/java/ml/gggrealms/gggmcanarchy/AnarchyPlugin.java
@@ -13,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
