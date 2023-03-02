de69781ce5af514c59ab64d43a0df48127a85266
GGG Dunlix <33298379+gggdunlix@users.noreply.github.com>
2023-03-02 09:17:03 -0500

    Update Skull.java

--- src/main/java/ml/gggrealms/gggmcanarchy/Skull.java
+++ src/main/java/ml/gggrealms/gggmcanarchy/Skull.java
@@ -1,8 +1,27 @@
 package ml.gggrealms.gggmcanarchy;
-import 
+import org.bukkit.entity.Player; 
+import java.util.UUID;
 public class Skull {
+  private UUID skullUUID;
+  private Player player;
+  private Location loc;
+  private UUID pUUID;
+  private int moneyAmount;
+  private int level;
   public Skull()
-  public Skull(Player owner, Location location, int money, int level) {
-    
+  public Skull(Player owner, Location location, int money, int lvl) {
+    skullUUID = UUID.randomUUID();
+    player = owner;
+    loc = location;
+    pUUID = owner.identity().uuid();
+    moneyAmount = money;
+    level = lvl;
+  }
+  public void registerSkull() {
+    FileConfiguration cfg = AnarchyPlugin.plugin.getConfigFile();
+    cfg.set("skulls." + skullUUID + ".owner", owner.getName());
+    cfg.set("skulls." + skullUUID + ".location", loc);
+    cfg.set("skulls." + skullUUID + ".amount", moneyAmount);
+    cfg.set("skulls." + skullUUID + ".level", owner.getName());
   }
 }
